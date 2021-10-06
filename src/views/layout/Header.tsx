c49c96eb583060bb630b3a477f1ef6955f4f41e1
Joseph Tseng <zxp930110@hotmail.com.tw>
2021-10-06 23:44:08 +0800

    create Header

---
+++ src/views/layout/Header.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyle = makeStyles((theme) => ({
+	header: {
+		minHeight: theme.layout.header.height,
+		width: '100%',
+		top: 0,
+		left: 0,
+		zIndex: theme.zIndex.appBar,
+		padding: '0.5em',
+		position: 'fixed',
+		display: 'grid',
+		placeItems: 'center',
+		backgroundColor: theme.palette.primary.main,
+		'& a': {
+			fontSize: '2em',
+			fontFamily: theme.typography.fontFamily,
+			color: theme.palette.common.white,
+		},
+	},
+}));
+
+export default function Header() {
+	const classes = useStyle();
+	return (
+		<header className={classes.header}>
+			<a href="/">{'Camping Menu'}</a>
+		</header>
+	);
+}
