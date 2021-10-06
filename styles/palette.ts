b808b2cc2b945ecdd1f39b2a7e5baea917c1d636
Joseph Tseng <zxp930110@hotmail.com.tw>
2021-10-07 01:48:45 +0800

    modify palette

--- styles/palette.ts
+++ styles/palette.ts
@@ -1,5 +1,32 @@
 import { Palette } from '@material-ui/core/styles/createPalette';
 
+declare module '@material-ui/core/styles/createPalette' {
+	// interface Palette {
+	// 	status: {
+	// 		red: string;
+	// 		orange: string;
+	// 		yellow: string;
+	// 		green: string;
+	// 		blue: string;
+	// 	};
+	// }
+	// interface PaletteOptions {
+	// 	status?: {
+	// 		red: string;
+	// 		orange: string;
+	// 		yellow: string;
+	// 		green: string;
+	// 		blue: string;
+	// 	};
+	// }
+
+	interface TypeBackground {
+		day1: string;
+		day2: string;
+		day3: string;
+	}
+}
+
 export const palette = {
 	primary: {
 		main: '#639763',
@@ -8,8 +35,11 @@ export const palette = {
 		main: '#ffbb2f',
 	},
 	background: {
-		default: '#f1ece8',
-		paper: '#1d2538',
+		default: '#fff',
+		paper: '#97bcc7',
+		day1: '#053d57',
+		day2: '#6a804e',
+		day3: '#6e8199',
 	},
 	error: {
 		main: '#ec5f5f',
