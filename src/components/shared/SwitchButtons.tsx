55ebb7b2eba1725db68b0fff10607f3731d86156
Joseph Tseng <zxp930110@hotmail.com.tw>
2021-10-07 23:18:45 +0800

    create switch button

--- src/components/shared/SwitchButtons.tsx
+++ src/components/shared/SwitchButtons.tsx
@@ -1,21 +1,56 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
+import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu';
 
-interface Props {
-	isRecipe: boolean;
-	setIsReceipe: React.Dispatch<React.SetStateAction<boolean>>;
+const useStyle = makeStyles(() => ({
+	switchButtons: {
+		top: '0.5em',
+	},
+	switchButton: {},
+}));
+
+interface SwitchButtonsProps {
+	isTrue: boolean;
+	setIsTrue: React.Dispatch<React.SetStateAction<boolean>>;
+	trueText: string;
+	falseText: string;
 }
 
-export default function SwitchButtons({ isRecipe, setIsReceipe }: Props) {
+export const SwitchButtons = ({ isTrue, setIsTrue, trueText, falseText }: SwitchButtonsProps) => {
+	const classes = useStyle();
+
 	return (
-		<ButtonGroup style={{ marginBlock: '1em' }}>
-			<Button color={'primary'} variant={isRecipe ? 'outlined' : 'contained'} onClick={() => setIsReceipe(false)}>
-				{'Stuff'}
+		<ButtonGroup className={classes.switchButtons}>
+			<Button color={'primary'} variant={isTrue ? 'outlined' : 'contained'} onClick={() => setIsTrue(false)}>
+				{falseText}
 			</Button>
-			<Button color={'primary'} variant={isRecipe ? 'contained' : 'outlined'} onClick={() => setIsReceipe(true)}>
-				{'Recipe'}
+			<Button color={'primary'} variant={isTrue ? 'contained' : 'outlined'} onClick={() => setIsTrue(true)}>
+				{trueText}
 			</Button>
 		</ButtonGroup>
 	);
+};
+
+interface SwitchButtonProps {
+	isTrue: boolean;
+	setIsTrue: React.Dispatch<React.SetStateAction<boolean>>;
 }
+
+export const SwitchButton = ({ isTrue, setIsTrue }: SwitchButtonProps) => {
+	const classes = useStyle();
+
+	return (
+		<Button
+			className={classes.switchButton}
+			variant={isTrue ? 'contained' : 'outlined'}
+			color={'secondary'}
+			onClick={() => {
+				setIsTrue((prev) => !prev);
+			}}
+		>
+			<RestaurantMenuIcon />
+		</Button>
+	);
+};
