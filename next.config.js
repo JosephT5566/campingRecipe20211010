52398495ba0c955e5f4eabeac1c4079c257e845a
Joseph Tseng <zxp930110@hotmail.com.tw>
2021-10-08 02:38:04 +0800

    set basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   reactStrictMode: true,
   images: {
     loader: "imgix",
