83c606982b30ed27437eae51d527b73621ef4c9b
Joseph Tseng <zxp930110@hotmail.com.tw>
2021-10-07 12:18:08 +0800

    set Typography style

---
+++ styles/typrogaphy.ts
@@ -0,0 +1,24 @@
+import { Typography } from '@material-ui/core/styles/createTypography';
+
+export const typography = {
+	fontFamily: 'Roboto, Arvo, Noto Sans TC',
+	h1: {
+		fontFamily: 'Roboto',
+		fontSize: '4rem',
+	},
+	h2: {
+        fontFamily: 'Arvo',
+        fontSize: '3rem',
+	},
+    h5: {
+        fontFamily: 'Arvo',
+    },
+
+	subtitle1: {
+		fontFamily: 'Arvo',
+	},
+
+	body1: {
+		fontSize: '1.25rem',
+	},
+} as Typography;
