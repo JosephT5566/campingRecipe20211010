242c8eaa86b8c2208c436ff507b1daff82b27ee8
Joseph Tseng <zxp930110@hotmail.com.tw>
2021-10-08 09:20:25 +0800

    modify deploy

--- next.config.js
+++ next.config.js
@@ -3,8 +3,4 @@ module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   reactStrictMode: true,
-  images: {
-    loader: "imgix",
-    path: "https://noop/",
-},
 }
